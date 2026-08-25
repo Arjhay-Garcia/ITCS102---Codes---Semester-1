5f7617d875b66bf0864734f81cd4762b4ddf2f18
Arjhay-Garcia <edu.arjhaygarcia@gmail.com>
2026-08-25 11:27:31 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,25 @@
+# Create a Python Script that generate the following:
+# 	Money to deposit: 1540
+#		something something
+
+money = 1540
+
+formula_result = 22
+
+bal = money-formula_result = 0
+
+
+
+print("Your current balance is", bal, "\n1000 =  \n", "500 = \n", "200 =  \n", "100 =  \n", " 50 =  \n", " 20 =  \n", " 10 =  \n", "  5 =  \n", "  1 =  \n")
+
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+
